eea87fe9d472db5134bf67d6b0da7ef9423c4ad9
apex <gloryofapex@gmail.com>
2025-07-23 13:31:35 +0800

    commit

---
+++ dist/hooks/useAnimatedComponents.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) JOB TODAY S.A. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { useRef } from "react";
+import { Animated } from "react-native";
+const INITIAL_POSITION = { x: 0, y: 0 };
+const ANIMATION_CONFIG = {
+    duration: 200,
+    useNativeDriver: true,
+};
+const useAnimatedComponents = () => {
+    const headerTranslate = useRef(new Animated.ValueXY(INITIAL_POSITION));
+    const footerTranslate = useRef(new Animated.ValueXY(INITIAL_POSITION));
+    const isVisible = useRef(true);
+    const setIsVisible = (shouldMakeVisible) => {
+        if (shouldMakeVisible) {
+            Animated.parallel([
+                Animated.timing(headerTranslate.current.y, {
+                    ...ANIMATION_CONFIG,
+                    toValue: 0,
+                }),
+                Animated.timing(footerTranslate.current.y, {
+                    ...ANIMATION_CONFIG,
+                    toValue: 0,
+                }),
+            ]).start(() => (isVisible.current = true));
+        }
+        else {
+            Animated.parallel([
+                Animated.timing(headerTranslate.current.y, {
+                    ...ANIMATION_CONFIG,
+                    toValue: -300,
+                }),
+                Animated.timing(footerTranslate.current.y, {
+                    ...ANIMATION_CONFIG,
+                    toValue: 300,
+                }),
+            ]).start(() => (isVisible.current = false));
+        }
+    };
+    const toggleIsVisible = () => {
+        setIsVisible(!isVisible.current);
+    };
+    const headerTransform = headerTranslate.current.getTranslateTransform();
+    const footerTransform = footerTranslate.current.getTranslateTransform();
+    return [
+        headerTransform,
+        footerTransform,
+        setIsVisible,
+        toggleIsVisible,
+    ];
+};
+export default useAnimatedComponents;
